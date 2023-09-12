a82377aa1223e71dab39b7a9aaa75ecd2ad80ad5
Alejo Calandra <alejocalandra18@gmail.com>
2023-09-12 16:58:45 -0300

    npm start

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validarCampos');
-const { cargarUsuarios } = require('../controllers/admin.controllers');
+const {
+	cargarUsuarios,
+	crearProducto,
+	cargarProductos,
+	eliminarProducto,
+	editarProducto,
+} = require('../controllers/admin.controllers');
 
 //va a ser el nombre del router que definamos
 const routerAdmin = express.Router();
 
 routerAdmin.get('/cargarUsuario', cargarUsuarios);
+
+routerAdmin.post(
+	'/new',
+	[
+		check('name', 'El nombre es obligatorio').not().isEmpty(),
+		check('precio', 'Por favor ingrese un valor').not().isEmpty(),
+		check('descripcion', 'la descripcion no es valida').isLength({
+			min: 10,
+		}),
+
+		validarCampos,
+	],
+	crearProducto
+);
+
+routerAdmin.get('/cargarProducto', cargarProductos);
+
+routerAdmin.delete('/eliminar/:id', eliminarProducto);
+
+routerAdmin.put('/editar', editarProducto);
 
 module.exports = routerAdmin;
